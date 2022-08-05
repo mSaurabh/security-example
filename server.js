c21542be791856db3fc376a1d8379d38507f4ca1
Saurabh Mankar <mankar.saurabh@gmail.com>
2022-08-05 16:24:42 -0500

    Adding dotenv for secret

--- server.js
+++ server.js
@@ -3,12 +3,36 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+require("dotenv").config();
+
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 
 // For security related middleware we use the middleware right at top
 app.use(helmet());
+//passport initialization
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; //TODO
